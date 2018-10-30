74b46b111cca7e6aebc7c09f60977a929c5f37e9
bdrum <boris.rumyantsev@cern.ch>
2018-10-30 16:30:06 +0300

    Simple CmakeTextList.txt was added; Using Spectrum class we can load ascii files and get arrays of values:   energy, channel number and counts;

--- inc/Spectrum.h
+++ inc/Spectrum.h
@@ -6,20 +6,63 @@
  
  
  /*!
-  * \brief Class for works with spectra arrays.
+  * \brief Class for works with spectrum.
   *
   * \author <a href="mailto:bdrum@jinr.ru">Boris Rumyantsev</a>
  */
 
+#include <Peak.h>
+#include <Background.h>
+#include <MetaData.h>
+
+#include <string>
+#include <fstream>
+#include <valarray>
+
+ //TODO: add interface for load spectrum from DB
+ //TODO: add try catch
+ //TODO: add logs via 
+ //TODO: add tests via googletest
+ //TODO: add docs
 class Spectrum {
+  // members:
   private:
-
-  public:
-
-    Spectrum(const *char cnfFileName);
-    Spectrum(std::string_view cnfFileName);
-
-    ClassDef(Spectrum, 1);
+    std::string extArr[6] = {".CSV", ".csv", ".TXT", ".txt", ".CNF", ".cnf"};
+    MetaData metaData;
+    const char defDelimiter = '\t';
+    std::valarray<int>* channelArr;
+    std::valarray<double>* energyArr;
+    std::valarray<int>* countsArr;
+    int printLevel;
+    
+  //methods:
+  private:
+    void LoadCnf(const std::string FileName, bool readMetaData);
+    void LoadAscii(const std::string FileName, bool readMetaData);
+    void Load(const std::string FileName, bool readMetaData);
+    // Spectrum* Load(const std::string FileName); - from DB
+    void ParseMetaDataCnf(const std::string&); // NOTE here perhaps should be not a string, but something like byte string
+    void ParseMetaDataAscii(const std::string&);
+    void ParseValue(const std::string);
+   public:
+    Spectrum();
+    Spectrum(const char delimiter);
+    Spectrum(const char* FileName, bool readMetaData=true) {Spectrum(); Load(std::string(FileName), readMetaData);};
+    void Load(const char* FileName, bool readMetaData=true) {return Load(std::string(FileName), readMetaData);};
+    //void Save(Spectrum* s, const char* FileName); // NOTE why is it needed? It would be better if I can add opportunity to save some temporary states of spectrum;
+    MetaData GetMetaData();
+    int GetPeaksCount();
+    Peak* GetPeaks();
+    Background* GetBackground();
+    std::valarray<int>*    GetChannelArray() {return channelArr;};
+    std::valarray<double>* GetEnergyArray()  {return energyArr;};
+    std::valarray<int>*    GetCountsArray()  {return countsArr;};
+    void SetChannelArray(std::valarray<int>* channelArr)  {this->channelArr = channelArr;};
+    void SetEnergyArray(std::valarray<double>* energyArr) {this->energyArr = energyArr;};
+    void SetCountsArray(std::valarray<int>* countsArr)    {this->countsArr = countsArr;};
+    void SetPrintLevel(int printLevel) {this->printLevel = printLevel;};
+    int  GetPrintLevel() {return printLevel;};
+    
 };
 
 #endif
